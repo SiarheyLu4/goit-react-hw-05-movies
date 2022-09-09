00b722fd558a2436b97494c3c9cc90cf6482b1be
Siarhei Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-09-09 16:26:45 +0300

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,31 @@
-import { lazy, Suspense } from "react";
+// import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { Menu } from "./Menu/Menu";
 import { Loader } from "./Loader/Loader";
-// import { HomePage } from "pages/HomePage/HomePage";
 
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
-const CastPage = lazy(() => import('../pages/CastPage/CastPage'));
-const AboutPage = lazy(() => import('../pages/AboutPage/AboutPage'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
-const ReviewsPages = lazy(() => import('../pages/ReviewsPages/ReviewsPages'));
+import { HomePage } from "pages/HomePage/HomePage";
+import { MoviesPage } from "pages/MoviesPage/MoviesPage";
+import { MovieDetailsPage } from "pages/MovieDetailsPage/MovieDetailsPage";
+import { CastPage } from "pages/CastPage/CastPage";
+import { ReviewsPages } from "pages/ReviewsPages/ReviewsPages";
+import { AboutPage } from "pages/AboutPage/AboutPage";
+import { NotFoundPage } from "pages/NotFoundPage/NotFoundPage";
+
+
+// const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+// const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+// const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
+// const CastPage = lazy(() => import('../pages/CastPage/CastPage'));
+// const AboutPage = lazy(() => import('../pages/AboutPage/AboutPage'));
+// const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+// const ReviewsPages = lazy(() => import('../pages/ReviewsPages/ReviewsPages'));
 
 export const App = () => {
   return (
     <div>
       <Menu />
-      <Suspense fallback={<p>{<Loader/> }</p>}>
+      {/* <Suspense fallback={<p>{<Loader/> }</p>}> */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
@@ -28,7 +36,7 @@ export const App = () => {
           <Route path="/about" element={<AboutPage />} />
           <Route path="*" element={<NotFoundPage/>} />
         </Routes>
-      </Suspense>
+      {/* </Suspense> */}
     </div>
   );
 };
